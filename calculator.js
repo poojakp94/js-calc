ebc33a37f02bdaf893fe9b4b804aa172add79cea
Pooja Prasad <poojakprasad94@gmail.com>
2019-04-20 21:54:54 +0530

    add initial files

---
+++ calculator.js
@@ -0,0 +1,14 @@
+ function dummyListener() {
+    document.getElementById('inputBox').value = "Hello world";
+ }
+
+ document.getElementById('button1').addEventListener('click', dummyListener);
+
+
+// const myObject = {
+//     name: 'bestObject',
+//     price: 50,
+//     getPrice: function () {
+//         console.log('Hello')
+//     }
+// }

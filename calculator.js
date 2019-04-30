bba712366e81b0f3e466673895a717e893cfb918
Pooja Prasad <poojakprasad94@gmail.com>
2019-04-30 20:31:11 +0530

    feat: working

--- calculator.js
+++ calculator.js
@@ -1,14 +1,29 @@
+// This is the event listener
 function onClickNum(value) {
-  console.log(value);
+  const inputValue = document.getElementById("inputBox").innerHTML; // accessing the div value
+  document.getElementById("inputBox").innerHTML = inputValue + value;
 }
 
 function addClickEvent(button) {
   const value = button.value;
-  button.addEventListener('click', function() {
+  button.addEventListener("click", function() {
     onClickNum(value);
   });
 }
 
-const buttons = document.getElementsByClassName('num-value'); // We get HTML collection of buttons (objects)
+function evaluateExpression() {
+  const expression = document.getElementById("inputBox").innerHTML;
+  var result = eval(expression);
+  document.getElementById("inputBox").innerHTML = result;
+}
+
+const buttons = document.getElementsByClassName("num-value"); // We get HTML collection of buttons (objects)
 const buttonsArray = Array.from(buttons);
 buttonsArray.forEach(addClickEvent);
+
+const equalButton = document.getElementById("equalId");
+equalButton.addEventListener("click", evaluateExpression);
+
+document.getElementById("clearButton").addEventListener("click", function() {
+  document.getElementById("inputBox").innerHTML = "";
+});
